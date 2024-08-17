d4b6882e3c67848e778fce251111c516c18f6577
TI Sabbir <tisabbir0@gmail.com>
2024-08-17 19:05:23 +0600

    vercel.json file added

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server
-    await client.connect();
+    // await client.connect();
 
     // Get the database and collection on which to run the operation
     const products = client.db("shopHubDB").collection("products");
@@ -83,7 +83,7 @@ async function run() {
     });
 
     // Ping MongoDB to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
